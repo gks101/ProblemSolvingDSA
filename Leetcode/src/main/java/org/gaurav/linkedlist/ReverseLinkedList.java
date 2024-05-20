03fcada4fecf68c0894529593a0f0dcc3a6ff601
Gaurav Kumar <forcewithgaurav@gmail.com>
2024-05-20 15:59:24 +0530

    Leetcode: 92. Reverse Linked List II

--- Leetcode/src/main/java/org/gaurav/linkedlist/ReverseLinkedList.java
+++ Leetcode/src/main/java/org/gaurav/linkedlist/ReverseLinkedList.java
@@ -12,4 +12,59 @@ public class ReverseLinkedList {
 
         return prev;
     }
+
+    /**
+     * Question: Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+     * Solution: 2 pass solution
+     * @param head Head of the linked list
+     * @param left Start position from which list to be reversed
+     * @param right End position till which list to be reversed
+     * @return  Head of the reversed list
+     */
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode leftToLeft = null;
+        ListNode rightToRight = null;
+        ListNode leftNode = null;
+        ListNode rightNode = null;
+
+        if (left == right) return head;
+
+        ListNode temp = head;
+        ListNode node = null;
+        int pos = 1;
+        while(temp != null) {
+            if (pos == left) {
+                leftNode = temp;
+                leftToLeft = node;
+            }
+            if (pos == right) {
+                rightNode = temp;
+                rightToRight = temp.next;
+                break;
+            }
+
+            node = temp;
+            temp = temp.next;
+            pos = pos + 1;
+        }
+
+        ListNode reverseHead = leftNode;
+        temp = leftNode;
+        node = null;
+        while (temp != rightToRight) {
+            ListNode nextNode = temp.next;
+            temp.next = node;
+            node = temp;
+            temp = nextNode;
+        }
+
+        if (leftToLeft != null) {
+            leftToLeft.next = node;
+        }
+        if (reverseHead != null) {
+            reverseHead.next = rightToRight;
+        }
+
+        return leftToLeft != null? head: node;
+    }
 }
